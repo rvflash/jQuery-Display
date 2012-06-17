42c7d5f6c7280f342f2a3b8519e0f64cb54bf928
rvflash <contact@rvdevsign.net>
2012-06-18 00:06:29 +0200

    Add samples and fix bug with variable passed by reference

--- README.md
+++ README.md
@@ -2,3 +2,37 @@ jQuery-Display
 ==============
 
 Give various methods to known if one DOM element is currently visible / displayed
+
+## Examples ##
+
+To apply action only if the element is currently displayed, you can now use:
+
+    $("#myId:display").css('background-color', 'red');
+
+And, you can specify that the element must be completely displayed: 
+
+    $("#myId:display(true)").css('color', 'black');
+
+Attach events when the element enters, is displayed or exits of the current view:
+
+    $("#myId")..display({
+        onEnter : function (elem){ console.log('Hello'); },
+        onView : function (elem){ console.log('Yes, I am here!'); },
+        onExit : function (elem){ console.log('Bye'); },
+    });
+
+And, two more attributes are available, one to specify that element is entirely displayed:
+
+    $("#myId")..display({
+        onExit : function (elem){ console.log($(elem).id + ' has been displayed'); },
+        fully : true,
+    });
+
+And the last to trigger events after time out: 
+
+    $("#myId")..display({
+        onEnter : function (elem){ console.log('... Hey!'); },
+        latency : 1000,
+    });
+
+As you seene, the DOM element has been passed as first parameter on each method. 
--- display.jquery.js
+++ display.jquery.js
@@ -13,61 +13,57 @@
 
     var display = function(elem, settings) {
         $(elem).each(function() {
+            var _self = this;
             var data = $.extend({
                 id : '_' + Math.floor((Math.random() * 1001) + 1),
                 displayed : false,
-            }, $(this).data('_display'));
+            }, $(_self).data('_display'));
 
-            if (displayed(this, settings.fully)) {
+            if (displayed(_self, settings.fully)) {
                 if (false == data.displayed && $.isFunction(settings.onEnter)) {
                     timers[data.id] = setTimeout(function() {
-                        settings.onEnter(this);
+                        settings.onEnter(_self);
                     }, settings.latency);
                 } else if ($.isFunction(settings.onView)) {
-                    settings.onView(this);
+                    settings.onView(_self);
                 }
                 data.displayed = true;
             } else if (data.displayed) {
                 if ($.isFunction(settings.onExit)) {
-                    settings.onExit(this);
+                    settings.onExit(_self);
                 }
                 clearTimeout(timers[data.id]);
                 data.displayed = false;
             }
-            $(this).data('_display', data);
+            $(_self).data('_display', data);
         });
     };
 
-    var displayed = function(elem, fully) {
-        if ($(elem).is(':visible')) {
-            var top = elem.offsetTop;
-            var left = elem.offsetLeft;
-            var width = elem.offsetWidth;
-            var height = elem.offsetHeight;
+    var displayed =
+            function(elem, fully) {
+                if ($(elem).is(':visible')) {
+                    var top = elem.offsetTop;
+                    var left = elem.offsetLeft;
+                    var width = elem.offsetWidth;
+                    var height = elem.offsetHeight;
 
-            while (elem.offsetParent) {
-                elem = elem.offsetParent;
-                top += elem.offsetTop;
-                left += elem.offsetLeft;
-            }
+                    while (elem.offsetParent) {
+                        elem = elem.offsetParent;
+                        top += elem.offsetTop;
+                        left += elem.offsetLeft;
+                    }
 
-            if ('undefined' == typeof (fully) || false == fully) {
-                return (
-                    top < (window.pageYOffset + window.innerHeight) && 
-                    left < (window.pageXOffset + window.innerWidth) && 
-                    (top + height) > window.pageYOffset && 
-                    (left + width) > window.pageXOffset
-                );
-            }
-            return (
-                top >= window.pageYOffset && 
-                left >= window.pageXOffset && 
-                (top + height) <= (window.pageYOffset + window.innerHeight) && 
-                (left + width) <= (window.pageXOffset + window.innerWidth)
-            );
-        }
-        return false;
-    };
+                    if ('undefined' == typeof (fully) || false == fully) {
+                        return (top < (window.pageYOffset + window.innerHeight)
+                                && left < (window.pageXOffset + window.innerWidth)
+                                && (top + height) > window.pageYOffset && (left + width) > window.pageXOffset);
+                    }
+                    return (top >= window.pageYOffset
+                            && left >= window.pageXOffset
+                            && (top + height) <= (window.pageYOffset + window.innerHeight) && (left + width) <= (window.pageXOffset + window.innerWidth));
+                }
+                return false;
+            };
 
     $.fn.display = function(settings) {
         var defaults = {
